66a5e1a4b22d8721b57658ff585e4b229c94ab51
polianaraujo <polianaellena123@gmail.com>
2023-03-20 11:11:56 -0300

    questao 1 e 4

--- questao1/main.c
+++ questao1/main.c
@@ -5,25 +5,25 @@ p=&i;
 q=&j;
 
 //&: endereço da variável
-//*: valor do endereço da variável
+//*: conteúdo do endereço da variável
 
-i=3, j=5;
-*p, *q;
-p=&i; // o conteúdo de p é igual ao endereço de i
-q=&j; // o conteúdo de q é igual ao endereço de j
+i=3, j=5; // i e j inteiros
+*p, *q;   // p e q ponteiros
+p=&i;     // o conteúdo de p é igual ao endereço de i
+q=&j;     // o conteúdo de q é igual ao endereço de j
 
 //Determine o valor das seguintes expressões:
 
 p==&i;
 //Resposta: p é igual ao endereço de i
 
-*p - *q;
-//o valor de p é o conteúdo de i
-//o valor de q é o conteúdo de j
-//Resposta: o valor de p - o valor de q
+*p - *q; //operação de subtração dos ponteiros
+//o conteúdo de p é o endereço de i
+//o conteúdo de q é o endereço de j
+//Resposta: o conteúdo de p - o conteúdo de q
 
 **&p;
-//Resposta: valor do valor do endereço de p
+//Resposta: conteúdo do conteúdo do endereço de p
 
 3 = *p/(*q) + 7;
-//Resposta: o valor de p dividido pelo valor de q + 7 é igual a 3
+//Resposta: o conteúdo de p dividido pelo conteúdo de q, adicionado 7 é igual a 3

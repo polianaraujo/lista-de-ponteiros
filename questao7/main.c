4b0b2639716566c93588106854bf8153a40f179f
polianaraujo <polianaellena123@gmail.com>
2023-04-19 21:47:59 -0300

    questao7

--- questao7/main.c
+++ questao7/main.c
@@ -9,9 +9,25 @@ Justifique.
 
 int main()
 {
-    int mat[4]={0,1,2,3}, *p, x;
+    int mat[4]={0,1,2,3}, *p1, *p2, *p3, x;
 
-    printf("%d", (mat+1));
+    //CORRETA
+    p1 = mat+1;       // p1 = mat[0 + 1] = mat[1] = 1
+    printf("%d", p1);
+    //Saída = 1
+
+    //INCORRETA
+    p2 = mat++;       //não existe essa expressão
+    //printf("%d", p2);
+
+    //INCORRETA
+    p3 = ++mat;       //não existe essa expressão
+    //printf("%d", p3);
+
+    //CORRETA
+    x = (*mat)++;     //x = endereço de mat + 1 = mat[0 + 1] = 1
+    printf("%d", x);
+    //Saída = 1
 
     return 0;
 }

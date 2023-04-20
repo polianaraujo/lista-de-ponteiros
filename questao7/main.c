605f8eff6c1ebac687a3bf7eb5c9dc0026b268c4
polianaraujo <polianaellena123@gmail.com>
2023-04-19 22:00:51 -0300

    questao7

--- questao7/main.c
+++ questao7/main.c
@@ -13,9 +13,9 @@ int main()
     //int  *p2, *p3;
     //CORRETA
     p = mat+1;       // p1 = mat[0 + 1] = mat[1] = 1
-    printf("%d", p);
+    printf("p = %d \n", p);
     //Saída = 1
-    printf("%d", mat[3]);
+    
     //INCORRETA
     //p2 = mat++;       //não existe essa expressão
     //printf("%d", p2);
@@ -26,7 +26,7 @@ int main()
 
     //CORRETA
     x = (*mat)++;     //x = endereço de mat + 1 = mat[0 + 1] = 1
-    printf("%d", x);
+    printf("x = %d", x);
     //Saída = 1
 
     return 0;

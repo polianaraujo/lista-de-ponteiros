b1633c6829cfb6dd5eac52b81d33a46f3291525d
polianaraujo <polianaellena123@gmail.com>
2023-04-29 18:47:14 -0300

    questao11

--- questao11/main.c
+++ questao11/main.c
@@ -9,10 +9,31 @@ int main()
     float aloha[10], coisas[10][5], *pf, value=2.2;
     int i=3;
 
-    aloha[2]=value;         //VÁLIDO
-    scanf("%f", &aloha);     //
-    aloha = value;           //
-    printf("%f", aloha);
-    coisas[4][4] = aloha[3];
+    aloha[2] = value;        //VÁLIDO
+    printf("%f\n", aloha[2]);
+    //aloha[2] = 2.2
+
+    printf("Insira um valor para o primeiro elemento de 'aloha': \n");
+    scanf("%f\n", &aloha);     //VÁLIDO
+    //&aloha = &aloha[0] = 'valor inserido pelo usuário'
+
+    //aloha = value;           //INVÁLIDO
+    //aloha = &aloha[0] = 2.2; mas um endereço deve ser inteiro e não decimal
+
+    printf("%f\n", aloha);     //VÁLIDO
+    //aloha = 2.2 (valor definido anteriormente, mas pode acabar sendo qualquer outro endereço)
+
+    coisas[4][4] = aloha[3];   //VÁLIDO
+    printf("")
+    //
+
+    //coisas[5] = aloha;       //
+    //
+
+    //pf = value;              //
+    //
+
+    //pf = aloha;              //
+    //
 }
 

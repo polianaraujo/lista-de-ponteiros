a4e615b2becafeacee08a0a37a90af676eb00a2c
polianaraujo <polianaellena123@gmail.com>
2023-04-29 14:53:52 -0300

    questao8

--- questao8/main.c
+++ questao8/main.c
@@ -25,16 +25,16 @@ int main()
 
     for(i=0;i<3;i++)
     {
-        printf("valor do %do (vet[%d]) elemento = %X \n", (i+1), i, (vet+i));
+        printf("valor do %do (vet[%d]) elemento = %X \n", i+1, i, vet+i);
     }
-    /*laço de repetição para imprimir os valores
-    percorrendo de 0 até 2 (três valores) elementos
+    /*laço de repetição para imprimir os endereços de cada
+    índice percorrendo de 0 até 2 (três endereços) elementos
     do vetor*/
 
     /*Saída:
-    valor do 1o (vet[0]) elemento = 4
-    valor do 2o (vet[1]) elemento = 9
-    valor do 3o (vet[2]) elemento = 13
+    valor do 1o (vet[0]) elemento = 61FF10
+    valor do 2o (vet[1]) elemento = 61FF14
+    valor do 3o (vet[2]) elemento = 61FF18
     */
     
     return 0;

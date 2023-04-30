73ba8b6a65e4ebe9fcc2d12341bfd1bd4262310c
polianaraujo <polianaellena123@gmail.com>
2023-04-30 08:42:15 -0300

    questao12

--- questao12/main.c
+++ questao12/main.c
@@ -3,3 +3,17 @@ Pesquise na Internet referências sobre o assunto
 e escreva um pequeno programa exemplificando o uso
 deste recurso.*/
 
+/*
+Numa máquina, as variáveis são interpretas como bits
+de 0s e 1s, e para conseguir interpretar elas de
+forma organizada e mais otimizada é através do endere-
+çamento que é único para cada bit.
+E é basicamente isso que um ponteiro é, um tipo de da-
+do que serve para indicar, ou armazenar, um endereço
+de memória(refer).
+Um ponteiro não é um inteiro, float, double ou char, 
+ele é um tipo que armazena o endereço em que o inte-
+iro está alocado (refer).
+
+https://www.cprogressivo.net/2013/03/O-que-sao-ponteiros-para-que-servem-e-como-usar-em-C.html
+*/

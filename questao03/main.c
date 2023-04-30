04bdc30992ec1a2bc7f83aa2a859c331abbb6079
polianaraujo <polianaellena123@gmail.com>
2023-04-30 09:15:54 -0300

    questao03

--- questao03/main.c
+++ questao03/main.c
@@ -4,11 +4,11 @@
  expressões de atribuição são ilegais?
 */
 
-p = i;     //atribuição legal
-q = &j;    //atribuição legal
-p = &*&i;  //atribuição legal
-i = (*&)j  //essa atribuição é ilegal
-i = *&j;   //atribuição legal
-i = *&*&j; //atribuição legal
-q = *p;    //atribuição legal
-i = (*p)++ + *q;
+p = i;           //atribuição legal
+q = &j;          //atribuição legal
+p = &*&i;        //atribuição legal
+i = (*&)j        //essa atribuição é ilegal
+i = *&j;         //atribuição legal
+i = *&*&j;       //atribuição legal
+q = *p;          //atribuição legal
+i = (*p)++ + *q; //atribuição legal

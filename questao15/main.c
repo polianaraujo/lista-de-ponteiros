3a8f465d172a417a09600863ff6f46d0f4ca50f1
polianaraujo <polianaellena123@gmail.com>
2023-04-30 11:26:04 -0300

    resolver questao15

---
+++ questao15/main.c
@@ -0,0 +1,8 @@
+/*
+15. Utilize a ideia do ponteiro para função pela função
+qsort() para implementar sua própria função de ordenação,
+mas que seja capaz de ordenar apenas inteiros do tipo int.
+Para isso, sua função deverá receber, entre outros argu-
+mentos, um ponteiro para a função de comparação que deter-
+minará como os elementos do array serão ordenados.
+*/

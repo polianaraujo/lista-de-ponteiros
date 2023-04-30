81f63fcb9fc33843ef78ff4190592a487fabec98
polianaraujo <polianaellena123@gmail.com>
2023-04-30 17:12:59 -0300

    pronto

--- questao11/main.c
+++ questao11/main.c
@@ -9,11 +9,9 @@ int main()
     float aloha[10], coisas[10][5], *pf, value=2.2;
     int i=3;
 
-    aloha[2] = value;        //VÁLIDO
-    printf("%f\n", aloha[2]);
+    aloha[2] = value;          //VÁLIDO
     //aloha[2] = 2.2
 
-    printf("Insira um valor para o primeiro elemento de 'aloha': \n");
     scanf("%f\n", &aloha);     //VÁLIDO
     //&aloha = &aloha[0] = 'valor inserido pelo usuário'
 
@@ -24,16 +22,14 @@ int main()
     //aloha = 2.2 (valor definido anteriormente, mas pode acabar sendo qualquer outro endereço)
 
     coisas[4][4] = aloha[3];   //VÁLIDO
-    printf("")
+    
+    //coisas[5] = aloha;       //INVÁLIDO
     //
 
-    //coisas[5] = aloha;       //
+    //pf = value;              //INVÁLIDO
     //
 
-    //pf = value;              //
-    //
-
-    //pf = aloha;              //
-    //
+    pf = aloha;                //VÁLIDO
+    //pf recebe o endereço do primeiro elemento de aloha
 }
 
---
+++ questao18/main.c
@@ -0,0 +1,96 @@
+/*
+18. Crie uma função capaz de realizar multiplicação matricial
+da forma C=AxB. A função deve receber 6 argumentos: os ponte-
+iros para as matrizes A, B e C, o número de linhas e colunas 
+de A e o número de colunas de B (assuma que o número de colu-
+nas de A é igual ao número de linhas de B). O resultado da mul-
+tiplicação deve ficar armazenado em C. Crie um programa para 
+testar sua implementação, capaz de utilizar a função de multi-
+plicação e imprimir as três matrizes. A função criada para mul-
+tiplicação não deve realizar nenhum tipo de saída de dados no 
+terminal. 
+Exemplo: para multiplicar duas matrizes (A e B) de dimensões 2x3
+e 3x4, respectivamente (o resultado deve ficar armazenado em C).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int al, ac, bl, bc, i, j;
+    //x = malloc(nl * sizeof(int*));
+    //x[0]=malloc(nc * nl * sizeof(int));
+
+    //definindo os tamanhos de A e B
+    printf("Defina as dimensoes de A = ");
+    scanf("%d %d", &al, &ac);
+    
+    printf("Defina as dimensoes de B = ");
+    scanf("%d %d", &bl, &bc);
+    
+    printf("A[%d][%d] e B[%d][%d]\n", al, ac, bl, bc);
+
+    int A[al][ac];
+    int B[bl][bc];
+    int C[ac][bl];
+
+    //definindo os valores da matriz A
+    printf("\nMatriz A:\n");
+    for(i=0; i<al; i++)
+    {
+        for(j=0; j<ac; j++)
+        {
+            A[i][j] = rand() % 10;
+            //printf("%d ", A[i][j]);
+        }
+        //printf("\n");
+    }
+    
+    //definindo os valores da matriz B
+    printf("\nMatriz B:\n");
+    for(i=0; i<bl; i++)
+    {
+        for(j=0; j<bc; j++)
+        {
+            B[i][j] = rand() % 10;
+            //printf("%d ", B[i][j]);
+        }
+        //printf("\n");
+    }
+    
+    //multiplicação de AxB, matriz C
+    //printf("\nMatriz C:\n");
+    for(i=0; i<al; i++)
+    {
+        for(j=0; j<bc; i++)
+        {
+            C[i][j] = 0;
+            int aux=0;
+            for(int x=0; x<bl; x++)
+            {
+                aux = A[i][x] * B[x][j];
+            }
+            C[i][j] = aux;
+            //printf("%d ", C[i][j]);
+        }
+        //printf("\n");
+    }
+
+
+    //imprimindo matriz C
+    printf("\nMatriz C:\n");
+    for(i=0; i<ac; i++)
+    {
+        for(j=0; j<bl; j++)
+        {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    //free(x[0]);
+    //free(x);
+
+    return 0;
+}
